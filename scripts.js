d4aa7dee1d2dfce345d182e2218d3f52cf8051a8
Jae Hwan Jung <46732186+JaeHJung@users.noreply.github.com>
2022-09-19 18:39:35 -0500

    Removed rotateX3D from the frame loop
    Removed rotateX3D from the frame loop

--- scripts.js
+++ scripts.js
@@ -175,24 +175,9 @@ function draw() {
 	delta = now - then;
 
 	if (delta > interval) {
-		// update time stuffs
-
-		// Just `then = now` is not enough.
-		// Lets say we set fps at 10 which means
-		// each frame must take 100ms
-		// Now frame executes in 16ms (60fps) so
-		// the loop iterates 7 times (16*7 = 112ms) until
-		// delta > interval === true
-		// Eventually this lowers down the FPS as
-		// 112*10 = 1120ms (NOT 1000ms).
-		// So we have to get rid of that extra 12ms
-		// by subtracting delta (112) % interval (100).
-		// Hope that makes sense.
-
 		then = now - (delta % interval);
 
 		clearCanvas();
-		rotateX3D(angle);
 		for (let i = 0; i < faces.length; i++) {
 			if (faceIsVisible(faces[i])) {
 				drawFace(faces[i], faceColors[i]);
@@ -200,7 +185,6 @@ function draw() {
 		}
 		drawVert();
 		drawEdge();
-		angle += 1;
 	}
 }
 
